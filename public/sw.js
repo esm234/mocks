b8dab9ad3ef2b7dc66ac1a8926f8851bdfda4e97
esm234 <97116875+esm234@users.noreply.github.com>
2025-07-30 07:43:28 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'pwa-cache-v2'; // Increment version for updates
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/index.css', // Assuming your CSS is bundled here
+  '/assets/index.js',  // Assuming your JS is bundled here
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting()) // Activate new service worker immediately
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+    .then(() => self.clients.claim()) // Take control of all clients immediately
+  );
+});
+
