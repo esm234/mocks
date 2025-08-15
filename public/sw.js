ce341b69e4e8b1b5f513ef6b4116b3b20a3ecdfc
esm234 <97116875+esm234@users.noreply.github.com>
2025-08-15 19:53:09 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v33'; // Increment version for updates
+const CACHE_NAME = 'pwa-cache-v45'; // Increment version for updates
 const urlsToCache = [
   '/',
   '/index.html',
